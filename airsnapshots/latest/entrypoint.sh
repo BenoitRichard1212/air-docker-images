2f95d952a6fa8d44a810e4d21d5a0fd0cca1e4b3
Reilly Lowery <luckylowery@gmail.com>
2019-06-07 05:45:53 -0400

    Update composer package for airsnapshots

--- airsnapshots/latest/entrypoint.sh
+++ airsnapshots/latest/entrypoint.sh
@@ -7,7 +7,7 @@ www_gid=`stat -c "%g" /var/www/html`
 echo "Host user is $www_uid:$www_gid"
 
 if [ ! $www_uid -eq 0 ]; then
-	echo "Updating airsnapshots user and group to match host IDs"
+	echo "Updating AirSnapshots user and group to match host IDs"
 	usermod -u $www_uid airsnapshots
 	groupmod -g $www_gid airsnapshots
 fi
