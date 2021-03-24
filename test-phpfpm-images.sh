dacd41f546b6e0c516b7c62c41a76f9719b7f93a
benoitrichard1212 <benoit.richard@novadba.com>
2021-03-24 17:51:13 -0400

    fix: added Dockerfile for 7.4/8.0

---
+++ test-phpfpm-images.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+children=""
+for version in $(ls -1 phpfpm)
+do
+  tmpFile=$(mktemp --tmpdir=. "php$version.XXX.log")
+  docker build --pull --no-cache -t "45air/phpfpm:$version" "phpfpm/$version" > $tmpFile 2>&1 &
+  pid="$!"
+  echo "Building $version (PID $pid, output in $tmpFile)..."
+  children+=" $version:$pid"
+done
+
+exitcode=0
+errorMsg="The following versions failed:"
+for versions in $children
+do
+	pid=${versions##*:}
+	ver=${versions%%:*}
+	if wait $pid ; then
+		rm php$ver.*.log
+	else
+		errorMsg+=" $ver"
+		exitcode=$(($exitcode+1))
+	fi
+done
+
+if (( $exitcode > 0 )); then
+	2>&1 echo $errorMsg
+	exit $exitcode
+fi
+
+
+docker build --pull --no-cache -t "45air/phpfpm:$version" "phpfpm/$version" > $tmpFile 2>&1
